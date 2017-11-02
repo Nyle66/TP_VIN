1836414bd2c6d8976dda75df65097a6bd11f27dd
alsina jeremy <myname@hotmail.fr>
2017-11-02 10:12:24 +0100

    ajout BDD

--- BDD.sql
+++ BDD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 02 Novembre 2017 à 10:00
+-- Généré le :  Jeu 02 Novembre 2017 à 10:11
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.1.1
 
@@ -35,6 +35,74 @@ INSERT INTO `categorie` (`id`, `name`, `description`) VALUES
 (2, 'Blanc', 'Tout les vins blanc'),
 (3, 'Rosé', 'Tout les vins rose');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_vendor`
+--
+
+CREATE TABLE `product_vendor` (
+  `vendeur_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `product_vendor`
+--
+
+INSERT INTO `product_vendor` (`vendeur_id`, `product_id`) VALUES
+(1, 1),
+(1, 3),
+(1, 5),
+(2, 2),
+(2, 4),
+(2, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` int(11) NOT NULL,
+  `vin` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `categorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `produit`
+--
+
+INSERT INTO `produit` (`id`, `vin`, `description`, `categorie_id`) VALUES
+(1, 'Languedoc', 'Vin rouge plutot fruité.', 1),
+(2, 'Bordeaux', 'Vin rouge plutot sec.', 1),
+(3, 'Rivesaltes', 'Vin blanc plutot fruité.', 2),
+(4, 'Chablis', 'Vin blanc plutot sec.', 2),
+(5, 'Miraval', 'Vin rosé plutot fruité.', 3),
+(6, 'Bartucci', 'Vin rosé plutot sec.', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vendeur`
+--
+
+CREATE TABLE `vendeur` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `vendeur`
+--
+
+INSERT INTO `vendeur` (`id`, `username`, `password`) VALUES
+(1, 'jeremy', 'pass'),
+(2, 'pierre', 'pass');
+
 --
 -- Index pour les tables exportées
 --
@@ -46,6 +114,18 @@ ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `vendeur`
+--
+ALTER TABLE `vendeur`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -54,3 +134,13 @@ ALTER TABLE `categorie`
 --
 ALTER TABLE `categorie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT pour la table `vendeur`
+--
+ALTER TABLE `vendeur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
